19ba66e039ff95b023b38b65c926d3361d1c5725
Ramanujamin <schmittw@umich.edu>
2017-02-23 17:42:12 -0500

    Initial Commit + some PKL files

---
+++ all_FAD_distances.py
@@ -0,0 +1,52 @@
+import numpy as np
+from biopandas import pdb
+import pandas as pd
+from FilterInteractions import *
+
+proteins = open('FADS.txt').readline().split('\n')[0].split(' ')
+
+def _make_distance_comparator(origin, tolerance=0.2):
+    keys = ["x_coord", "y_coord", "z_coord"]
+    vdW_keyatom = get_vdW_radius(origin['atom_name'].values[0])
+    coords = origin[keys].values[0]
+    x_o, y_o, z_o = coords[0], coords[1], coords[2]
+
+    def distance_comparator(point):
+        coords = point[keys]
+        x, y, z = coords[0], coords[1], coords[2]
+        net_x, net_y, net_z = abs(x - x_o), abs(y - y_o), abs(z - z_o)
+        if net_x < vdW_bounds['lower'] and net_y < vdW_bounds['lower'] and net_z < vdW_bounds['lower']:
+            distance = np.sqrt(net_x **2 + net_y ** 2 + net_z ** 2)
+            expected_weak_iteraction_dist = get_vdW_radius(point['atom_name']) + vdW_keyatom
+            if distance < expected_weak_iteraction_dist + tolerance and distance > expected_weak_iteraction_dist - tolerance:
+                return distance
+        return np.nan
+
+    return distance_comparator
+
+key_atoms = ['C10', 'O4']
+
+for protein in proteins:
+    atmnums = []
+    pro = pdb.PandasPDB().fetch_pdb(protein).df
+    pro = pd.concat([pro['ATOM'], pro["HETATM"]])
+
+    for key in key_atoms:
+        for x in pro[pro['atom_name'] == key].atom_number:
+            atmnums.append(x)
+
+    neighbours = []
+    for num in atmnums:
+        atom_data = pro[pro.atom_number == num]
+        func = _make_distance_comparator(atom_data)
+        distance_from_atom_df = pro.apply(func, axis=1)
+        pro['distance'] = distance_from_atom_df
+        valid_atom_indices = pro['distance'].notnull()
+        valid_key_atoms = pro[valid_atom_indices].sort_values(by=['distance'], ascending=True)
+        neighbours.append(valid_key_atoms)
+
+    df = pd.DataFrame()
+    df['atom_number'] = atmnums
+    df['neighbors'] = neighbours
+    df.to_pickle(protein + '.pkl')
+
